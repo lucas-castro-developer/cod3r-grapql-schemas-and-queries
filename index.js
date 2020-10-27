3c6452d13ac2474e118caaffa4efc25f640f742d
Lucas Castro <lucas.sc98@hotmail.com>
2020-10-26 23:19:33 -0300

    Challenge - The time is now

--- index.js
+++ index.js
@@ -1,25 +1,34 @@
-const { AppoloServer, gql, ApolloServer } = require('apollo-server')
+const { AppoloServer, gql, ApolloServer } = require("apollo-server");
 
 const typeDefs = gql`
-    # Pontos de entrada da sua API
-    type Query {
-        ola: String
-    }
-`
+  # Pontos de entrada da sua API
+  type Query {
+    ola: String
+    horaAtual: String
+  }
+`;
 
 const resolvers = {
-    Query: {
-        ola() {
-            return 'Olá GraphQL!'
-        }
-    }
-}
+  Query: {
+    ola() {
+      return "Olá GraphQL!";
+    },
+    horaAtual() {
+      const dataHora = new Date();
+      const horaAtual = dataHora.getHours();
+      const minutoAtual = dataHora.getMinutes();
+      const segundoAtual = dataHora.getSeconds();
+      const horaCerta = `${horaAtual}h:${minutoAtual}min:${segundoAtual}s`;
+      return horaCerta;
+    },
+  },
+};
 
 const server = new ApolloServer({
-    typeDefs,
-    resolvers
-})
+  typeDefs,
+  resolvers,
+});
 
 server.listen().then(({ url }) => {
-    console.log(`Executando em ${url}`)
-})
+  console.log(`Executando em ${url}`);
+});
