2321ca7111fdc82861bc2c37ff31ad69a327b9b7
Lucas Castro <lucas.sc98@hotmail.com>
2020-10-26 22:58:22 -0300

    Configuring Apollo Server

---
+++ index.js
@@ -0,0 +1,18 @@
+const { AppoloServer, gql, ApolloServer } = require('apollo-server')
+
+const typeDefs = gql`
+
+`
+
+const resolvers = {
+    
+}
+
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+server.listen().then(({ url }) => {
+    console.log(`Executando em ${url}`)
+})
